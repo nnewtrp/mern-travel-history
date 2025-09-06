3839243d3eea0347149c7d4bfb2cd016843b1013
nnewtrp <newnack2013@gmail.com>
2025-09-06 22:49:19 +0700

    Install packages and connect backend to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -9,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 // Connect to MongoDB
-// mongoose.connect('mongodb://localhost/mern-stack-db', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Define routes and middleware
 app.listen(PORT, () => {
