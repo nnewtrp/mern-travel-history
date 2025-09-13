5371e4c82697460e33dfcd94922116ba81cb2767
nnewtrp <newnack2013@gmail.com>
2025-09-13 14:57:33 +0700

    Set new route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'leaflet/dist/leaflet.css'
-import HomePage from "./pages/HomePage.tsx";
+import MapPage from "./pages/MapPage.tsx";
 import './App.css'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/new" element={<HomePage />} />
+        {/* <Route path="/" element={<LoginPage />} /> */}
+        <Route path="/map" element={<MapPage />} />
+        {/* <Route path="/manage" element={<ManagePage />} /> */}
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/layouts/NavBar.tsx
+++ frontend/src/components/layouts/NavBar.tsx
@@ -1,12 +1,66 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, Divider, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import MenuDropdownIcon from "./MenuDropDownIcon";
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function NavBar() {
+  const navigate = useNavigate()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const menuList = [
+    { title: 'Home', path: '/' },
+    { title: 'Add Trip', path: '/manage' },
+  ]
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleMenuOnClick = (path: string) => {
+    navigate(path)
+    handleClose()
+  }
+
+
   return (
     <AppBar sx={{ width: '100%' }}>
       <Toolbar>
         <Typography variant="h6">Travel History</Typography>
         <div style={{ flexGrow: 1 }} />
-        <Typography variant="body1">User</Typography>
+        
+        {/* Menu Drop Down */}
+        <Button variant="text" onClick={handleMenu} sx={{ color: "white", fontWeight: "bold" }}>
+          Menu <MenuDropdownIcon isRotate={!!anchorEl} />
+        </Button>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          sx={{ mt: "35px" }}
+          open={!!anchorEl}
+          onClose={handleClose}
+        >
+          {menuList.map((menu) => (
+            <div key={menu.title}>
+              <MenuItem onClick={() => handleMenuOnClick(menu.path)} sx={{ fontWeight: "bold" }}>
+                {menu.title}
+              </MenuItem>
+              <Divider />
+            </div>
+          ))}
+        </Menu>
       </Toolbar>
     </AppBar>
   )
